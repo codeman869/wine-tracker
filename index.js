20cc9f693b51b0336cff837eee2dc8a740c2ce37
unknown <me@codydeckard.com>
2025-07-14 13:52:17 -0700

    reorganized files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path  = require('node:path');
 const express = require('express');
 const cors  = require('cors');
 const { Pool } = require('pg');
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.port || 5000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
 app.use(express.json());
 
 const pool = new Pool({
@@ -18,5 +20,9 @@ app.get('/api/wines', async (req,res) => {
     const result = await pool.query('SELECT * FROM wines');
     res.json(result.rows);
 });
+
+app.get(/(.*)/, (req,res) => (
+    res.sendFile(path.join(__dirname, 'client','dist', 'index.html'))
+));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
